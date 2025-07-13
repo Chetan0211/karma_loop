e7d481ebc0253b80e9cd521e8d2b1a62a3c43ac1
Chetan0211 <saichetanreddy0211@gmail.com>
2025-07-14 01:26:46 +0530

    Fix Errors

--- app/concepts/comment/reaction.rb
+++ app/concepts/comment/reaction.rb
@@ -3,7 +3,6 @@ class Comment::Reaction < Trailblazer::Operation
   step Contract::Build(constant: Comment::Contract::Reaction)
   step Contract::Validate()
   step Contract::Persist()
-  step :update_count
 
   def model_setup(options, comment_reaction:, **)
     options[:model] = CommentsReaction.find_or_initialize_by(
@@ -12,12 +11,5 @@ class Comment::Reaction < Trailblazer::Operation
       comment_id: comment_reaction[:comment_id]
     )
     options[:model].reaction = comment_reaction[:reaction]
-  end
-
-  def update_count(options, **)
-    comment = Comment.find(options[:comment_reaction][:comment_id])
-    likes = CommentsReaction.where(comment_id: comment.id, reaction: 'like').count
-    dislikes = CommentsReaction.where(comment_id: comment.id, reaction: 'dislike').count
-    comment.update(likes: likes, dislikes: dislikes)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,7 @@ class User < ApplicationRecord
   end
 
   def all_chat_groups
-    Group.where(type: %w[friend group]).joins(:group_users, :chats).where(group_users:{user_id: self.id}).uniq
+    Group.where(type: %w[friend group]).joins(:group_users).where(group_users:{user_id: self.id}).includes(:chats).uniq
   end
 
   def following?(user)
