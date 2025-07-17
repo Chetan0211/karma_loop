98124d8e2ddadb876afa8bb57f201a0ac13c65f6
Chetan0211 <saichetanreddy0211@gmail.com>
2025-07-17 23:24:18 +0530

    Fix ability issue for accessing post

--- app/models/ability/post.rb
+++ app/models/ability/post.rb
@@ -2,7 +2,7 @@ class Ability::Post
   include CanCan::Ability
   def initialize(user)
     can [:manage, :create_comment, :read_comment], Post, user_id: user.id
-    can [:read, :create_comment, :read_comment], Post, status: "published", scope: :public, deleted_at: nil
+    can [:read, :create_comment, :read_comment], Post, status: "published", scope: "public", deleted_at: nil
     can [:read, :create_comment, :read_comment], Post, status: "published", user_id: user.all_following.pluck(:id), deleted_at: nil
 
     can [:update_comment, :destroy_comment], Comment, commenter_id: user.id
