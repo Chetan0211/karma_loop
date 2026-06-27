3a23ecdb3db7a3da746d3b62dccbd41fdfa825d1
Chetan0211 <saichetanreddy0211@gmail.com>
2026-06-27 12:24:02 +0000

    Fix elastic search URL in deploy file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV['BREVO_SMTP_SERVER']  ,
+    address:              ENV['BREVO_SMTP_SERVER'],
     port:                 ENV['BREVO_SMTP_PORT'],
     domain:               ENV['BREVO_DOMAIN'],
     user_name:            ENV['BREVO_SMTP_USER_NAME'],
