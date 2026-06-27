b6f9512553bc38e09ab3920dc4f32ec1d5d73ab4
Chetan0211 <saichetanreddy0211@gmail.com>
2026-06-27 15:48:02 +0000

    KAR-71: Fix chat and posting bug issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_02_045543) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_27_151052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -192,7 +192,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_02_045543) do
     t.datetime "deleted_at"
     t.string "display_name", limit: 30, null: false
     t.string "bio", limit: 200
-    t.string "scope", default: "public", null: false
+    t.string "scope", default: "private", null: false
     t.string "encrypted_key", null: false
     t.string "public_key", null: false
     t.datetime "last_key_reset_at"
